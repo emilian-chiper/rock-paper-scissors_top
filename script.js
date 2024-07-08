a7224c14b95b254a872463bd05b12c81f1471d7d
emiliano <emilian.chiper.developer@gmail.com>
2024-07-08 18:43:09 +0300

    Write the logic to get the human choice

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+"use strict";
+
 // 1. Write the logic to get computer choice
 const getComputerChoice = function () {
-  const elems = ["rock", "paper", "scissors"];
-  const choice = Math.floor(Math.random() * elems.length);
-  return elems[choice];
+  const weapons = ["rock", "paper", "scissors"];
+  const computerChoice = Math.floor(Math.random() * weapons.length);
+  return weapons[computerChoice];
 };
 
 getComputerChoice();
+
+// 2. Write the logic to get the human choice
+const getHumanChoice = function () {
+  const humanChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
+  return humanChoice;
+};
+
+console.log(getHumanChoice());
