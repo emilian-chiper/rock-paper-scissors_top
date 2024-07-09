88fe5ca826b8d17cddb36e4394aa3325045b62e8
emiliano <emilian.chiper.developer@gmail.com>
2024-07-09 10:38:38 +0300

    Write the logic for playing one round

--- script.js
+++ script.js
@@ -15,8 +15,33 @@ const getHumanChoice = function () {
   return humanChoice;
 };
 
-console.log(getHumanChoice());
-
 // 3. Declare the players score variables
-const humanScore = 0,
+let humanScore = 0,
   computerScore = 0;
+
+// 4. Write the logic to play a single round
+const playRound = function (humanChoice, computerChoice) {
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+
+  console.log(humanChoice, computerChoice);
+
+  if (humanChoice === computerChoice) console.log(`It's a tie`);
+
+  const winningCases = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
+
+  const result =
+    humanChoice === winningCases[computerChoice]
+      ? "Computer wins!"
+      : "You win!";
+
+  result === "You win!" ? humanScore++ : computerScore++;
+
+  console.log(result, humanScore, computerScore);
+};
+
+playRound();
