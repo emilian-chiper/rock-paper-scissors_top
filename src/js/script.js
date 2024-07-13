812b8eedbced0b868ec460703f6cc5ef3b5c184d
emiliano <emilian.chiper.developer@gmail.com>
2024-07-13 19:15:17 +0300

    Import and store DOM elements

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const ROCK = "rock";
     const PAPER = "paper";
     const SCISSORS = "scissors";
+
+    // DOM ELEMENTS
+    const roundsElement = document.querySelector(".rounds");
+    console.log(roundsElement);
+
+    const choiceElements = [...document.querySelectorAll(".__choice")];
+    console.log(choiceElements);
+
+    const scoreElements = [...document.querySelectorAll(".__score")];
+    console.log(scoreElements);
+
+    const promptElement = document.querySelector(".prompt");
+    console.log(promptElement);
+
+    const playButtons = [...document.querySelectorAll(".__play")];
+    console.log(playButtons);
   };
 
   main();
